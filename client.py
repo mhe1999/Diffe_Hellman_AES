c0954af9b13a856b39bcd859b98b80bb0cbed25b
mhe1999 <ehteshami1378@gmail.com>
2020-05-16 22:12:31 +0430

    add exchanging a private session key with Diffie-hellman algorithm

--- client.py
+++ client.py
@@ -1,11 +1,34 @@
 import socket
+import re
+
+cilent_private = 0x7250f5b473a13f2faffa851c4076bc2c
+
+
 
 HOST = '127.0.0.1'  # The server's hostname or IP address
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    while True:
+        data = s.recv(1024)
+        if not data:
+            continue
+        data = data.decode()
+        print('Received -------> "', data, '"')
 
-print('Received', repr(data))
+        if data.startswith('INITIAL_PARAMS'):
+            prime = int(re.findall('prime:([0-9]+),', data)[0])
+            g = int(re.findall('g:([0-9]+)', data)[0])
+            print('prime:' , hex(prime))
+            print('g:' , hex(g))
+            client_public = (g ^ cilent_private) % prime
+            data = 'CLIENT_PUBLIC_KEY =' + str(client_public)
+            print('sending the client public key to server...')
+            s.sendall(data.encode())
+
+        if data.startswith('SERVER_PUBLIC_KEY'):
+            server_public = int(re.findall('[0-9]+' , data)[0])
+            print('server public key:' , hex(server_public))
+            session_key = (server_public ^ cilent_private) % prime
+            print("session_key:" , hex(session_key))
--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 import socket
+import re
+
+#server generate a prime number and generater(for example with openssl)
+prime = 0xba01af369bef860023562c7f5e517a9b
+g = 2
+server_private = 0x4de0438f4457df470dd099a3c108a9cc
+server_public = (g ^ server_private) % prime
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
@@ -9,8 +16,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
+        print('sending the public paramters of Diffie-hellman to client...\n\n')
+        data = 'INITIAL_PARAMS = prime:' + str(prime) + ', g:' + str(g)
+        conn.sendall(data.encode())
+        print('sending the sever public key to client...\n\n')
+        data = 'SERVER_PUBLIC_KEY =' + str(server_public)
+        conn.sendall(data.encode())
         while True:
             data = conn.recv(1024)
             if not data:
-                break
-            conn.sendall(data)
+                continue
+            data = data.decode()
+            if data.startswith('CLIENT_PUBLIC_KEY'):
+                client_public = int(re.findall('[0-9]+' , data)[0])
+                print('client public key:' , hex(client_public))
+                session_key = (client_public ^ server_private) % prime
+                print("session_key:" , hex(session_key))
