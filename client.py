387861d7383fc8792fae8f5f8cf716222046a60e
mhe1999 <ehteshami1378@gmail.com>
2020-05-17 04:37:31 +0430

    exchange encrypted messages with AES

--- client.py
+++ client.py
@@ -1,12 +1,34 @@
 import socket
 import re
+import base64
+from Crypto.Cipher import AES
 
-cilent_private = 0x7250f5b473a13f2faffa851c4076bc2c
+def base64_encode(message_bytes):
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
 
+def base64_decode(base64_message):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes
 
+def encryption(session_key, plaintext):
+    cipher = AES.new(session_key.to_bytes(16, byteorder = 'big'),AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode()) # encryte data
+    return ciphertext, nonce
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
+def decryption(session_key, message_bytes, nonce):
+    cipher = AES.new(session_key.to_bytes(16, byteorder = 'big'), AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(message_bytes)               # decrype message with session key and nonce
+    plaintext = plaintext.decode()
+    return plaintext
+
+cilent_private = 0x7250f5b473a13f2faffa851c4076bc2c # client private key
+
+HOST = '127.0.0.1'
+PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
@@ -17,18 +39,45 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         data = data.decode()
         print('Received -------> "', data, '"')
 
-        if data.startswith('INITIAL_PARAMS'):
+        if data.startswith('INITIAL_PARAMS'):                   # Receiving initial parameters
             prime = int(re.findall('prime:([0-9]+),', data)[0])
             g = int(re.findall('g:([0-9]+)', data)[0])
+
             print('prime:' , hex(prime))
             print('g:' , hex(g))
-            client_public = (g ^ cilent_private) % prime
+
+            client_public = (g ^ cilent_private) % prime        # calculate public key
             data = 'CLIENT_PUBLIC_KEY =' + str(client_public)
-            print('sending the client public key to server...')
+            print("\nsending the client's public key to server...\n") # send client's public key without encryption
             s.sendall(data.encode())
 
-        if data.startswith('SERVER_PUBLIC_KEY'):
+        if data.startswith('SERVER_PUBLIC_KEY'):                # Receiving servers's public key
             server_public = int(re.findall('[0-9]+' , data)[0])
+            print('client public key:' , hex(client_public))
+            print('client private key:', hex(cilent_private))
             print('server public key:' , hex(server_public))
-            session_key = (server_public ^ cilent_private) % prime
-            print("session_key:" , hex(session_key))
+            session_key = (server_public ^ cilent_private) % prime  # calculate session key base on server's public key and client's private key
+            print("session_key:" , hex(session_key), '\n')
+
+            plaintext = input('Enter your text to send\n')
+            ciphertext, nonce = encryption(session_key, plaintext)
+
+            base64_message = base64_encode(ciphertext)
+            base64_message_nonce = base64_encode(nonce)
+            print('\nthe plain text is  :', plaintext)
+            print('the cipher text is :', base64_message)
+            print('sending \'', base64_message, '\' (cipher text) to server...\n')
+            data = 'CIPHER_TEXT:' + str(base64_message) + ', NONCE:' + str(base64_message_nonce) # send encryted data to server
+            s.sendall(data.encode())
+
+        if data.startswith('CIPHER_TEXT :'):                          # Receiving server's cipherText which encryted with session key
+            print('Received -------> "', data, '"')
+            ciphertext = re.findall('CIPHER_TEXT :(.+),', data)[0]
+            nonce = re.findall('NONCE:(.+)', data)[0]
+            print('\nraw cipher text of Received message: ', ciphertext)
+
+            message_bytes = base64_decode(ciphertext)
+            message_bytes_nonce = base64_decode(nonce)              # convert bsae64 nonce and cipherText to bytes
+            plaintext = decryption(session_key, message_bytes, message_bytes_nonce)
+            print('the dectypted message of Received message: ', plaintext)
+            break
--- server.py
+++ server.py
@@ -1,14 +1,39 @@
 import socket
 import re
+import base64
+from Crypto.Cipher import AES
 
-#server generate a prime number and generater(for example with openssl)
+def base64_encode(message_bytes):
+    base64_bytes = base64.b64encode(message_bytes)
+    base64_message = base64_bytes.decode('ascii')
+    return base64_message
+
+def base64_decode(base64_message):
+    base64_bytes = base64_message.encode('ascii')
+    message_bytes = base64.b64decode(base64_bytes)
+    return message_bytes
+
+def encryption(session_key, plaintext):
+    cipher = AES.new(session_key.to_bytes(16, byteorder = 'big'),AES.MODE_EAX)
+    nonce = cipher.nonce
+    ciphertext, tag = cipher.encrypt_and_digest(plaintext.encode()) # encryte data
+    return ciphertext, nonce
+
+def decryption(session_key, message_bytes, nonce):
+    cipher = AES.new(session_key.to_bytes(16, byteorder = 'big'), AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(message_bytes)               # decrype message with session key and nonce
+    plaintext = plaintext.decode()
+    return plaintext
+
+# server generate a prime number and generater(for example with openssl)
 prime = 0xba01af369bef860023562c7f5e517a9b
 g = 2
-server_private = 0x4de0438f4457df470dd099a3c108a9cc
-server_public = (g ^ server_private) % prime
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
+server_private = 0x4de0438f4457df470dd099a3c108a9cc # server private key
+server_public = (g ^ server_private) % prime    # calculate public key
+
+HOST = '127.0.0.1'
+PORT = 65432
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
@@ -16,19 +41,49 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     conn, addr = s.accept()
     with conn:
         print('Connected by', addr)
-        print('sending the public paramters of Diffie-hellman to client...\n\n')
-        data = 'INITIAL_PARAMS = prime:' + str(prime) + ', g:' + str(g)
+        print('\nsending the public paramters of Diffie-hellman to client...\n')
+        data = 'INITIAL_PARAMS = prime:' + str(prime) + ', g:' + str(g) # send two public paramets(prime number and generator) without encryption
         conn.sendall(data.encode())
-        print('sending the sever public key to client...\n\n')
+
+        print("sending the server's public key to client...\n")          # send server's public key without encryption
         data = 'SERVER_PUBLIC_KEY =' + str(server_public)
         conn.sendall(data.encode())
+
         while True:
             data = conn.recv(1024)
             if not data:
                 continue
             data = data.decode()
-            if data.startswith('CLIENT_PUBLIC_KEY'):
+            if data.startswith('CLIENT_PUBLIC_KEY'):                    # Receiving client public key
+                print('Received -------> "', data, '"')
                 client_public = int(re.findall('[0-9]+' , data)[0])
+                print('server public key:' , hex(server_public))
+                print('server private key:' , hex(server_private))
                 print('client public key:' , hex(client_public))
-                session_key = (client_public ^ server_private) % prime
-                print("session_key:" , hex(session_key))
+
+                session_key = (client_public ^ server_private) % prime  # calculate session key base on client public key and server private key
+                print("session_key:" , hex(session_key), '\n\n')
+
+            if data.startswith('CIPHER_TEXT'):                          # Receiving client cipherText which encryted with session key
+                print('Received -------> "', data, '"')
+                ciphertext = re.findall('CIPHER_TEXT:(.+),', data)[0]
+                nonce = re.findall('NONCE:(.+)', data)[0]
+                print('raw cipher text of Received message: ', ciphertext)
+
+                message_bytes = base64_decode(ciphertext)
+                message_bytes_nonce = base64_decode(nonce)              # convert bsae64 nonce and cipherText to bytes
+                plaintext = decryption(session_key, message_bytes, message_bytes_nonce)
+                print('the dectypted message of Received message: ', plaintext)
+
+                plaintext = input('\nEnter your text to send\n')
+                ciphertext, nonce = encryption(session_key, plaintext)
+
+                base64_message = base64_encode(ciphertext)
+                base64_message_nonce = base64_encode(nonce)
+                print('\nthe plain text is  :', plaintext)
+                print('the cipher text is :', base64_message)
+                print('sending \'', base64_message, '\' (cipher text) to client...\n')
+
+                data = 'CIPHER_TEXT :' + str(base64_message) + ', NONCE:' + str(base64_message_nonce) # send encryted data to client
+                conn.sendall(data.encode())
+                break
